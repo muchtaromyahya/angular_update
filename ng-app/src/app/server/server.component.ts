606a30bf12bc253c5321d31e4ff6dfe1c4c3df45
muchtaromyahya <muchtaromyahya@gmail.com>
2020-04-11 20:59:47 +0700

    string interpolation

--- ng-app/src/app/server/server.component.ts
+++ ng-app/src/app/server/server.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId : number = 10;
+  serverStatus: string = 'sibuk';
 
   constructor() { }
 
